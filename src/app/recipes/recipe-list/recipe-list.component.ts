d0be731c4b1a3f11f5d3d8c9548c032c0e44f4e6
ALESSANDRO VIKRAM <alexandervikram80@gmail.com>
2020-12-02 12:28:31 +0530

    large changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes=[];
+  recipes:Recipe[]=[new Recipe('Test Recipe','Chicken Soup','https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-fried-rice-009-1543877224.jpg?crop=1xw:1xh;center,top&resize=768:*')];
 
   constructor() { }
 
